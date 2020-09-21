8b57eac7ceb9cf7ad73b46ca827db41a5b94b04b
YassinYassinYassinYassin <70814923+YassinYassinYassinYassin@users.noreply.github.com>
2020-09-21 10:05:54 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var car, wall;
+var speed, weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(55,90);
+  weight=random(400,1500);
+  
+  car = createSprite(50, 200, 50, 50);
+
+  wall=createSprite(1500, 200, 60, 200);
+  wall.shapeColor = color(80, 80, 80);
+}
+
+
+function draw() {
+  background(255,255,255);  
+
+  car.velocityX = speed;
+
+
+
+  drawSprites();
+}
